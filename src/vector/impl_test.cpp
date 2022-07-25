ed7c95189d0b9e3587cd7dd9864c6afe21ad978d
kimhxsong <kimhxsong@gmail.com>
2022-07-25 14:02:08 +0900

    Fix vector.insert

--- src/vector/impl_test.cpp
+++ src/vector/impl_test.cpp
@@ -1,61 +1,39 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
+#include <string>
 
 class A {
- public:
-    A() {
-        std::cout << "A default" << std::endl;
-    }
-    A(int n) : n(n) {
-        std::cout << "A constructor: " << this->n << std::endl;
-    }
-    A(const A& other) : n(other.n) {
-        std::cout << "A copied: " << this->n << std::endl;
-    } 
-    virtual ~A() {
-        std::cout << "A destructor: " << this->n << std::endl;
-    }
-    void doSomething() {}
-    int n;
-};
+    int num;
 
-class B : public A {
  public:
-    B() {
-        std::cout << "B default" << std::endl;
+    A() : num(0) {
+        std::cout << "ctor: default" << std::endl;
     }
-    B(int n) : n(n) {
-        std::cout << "B constructor: " << this->n << std::endl;
+
+    A(int num) : num(num) {
+        std::cout << "ctor: conversion" << std::endl;
     }
-    virtual ~B() {
-        std::cout << "B destructor: " << this->n << std::endl;
+
+    A(const A& other) : num(other.num) {
+        std::cout << "ctor: copied" << std::endl;
     }
-    int n;
+
+    ~A() {
+        std::cout << "dtor" << std::endl;
+    }
+
+    A& operator=(const A& other) {
+        std::cout << "oper: =" << std::endl;
+        return *this;
+    }
 };
 
 int main() {
-{   
-    std::vector<A> alist(10);  // 기본 생성자 호출 10회, 사이즈 10.
-    alist.reserve(20);  // 소멸자 호출 10회, 생성자 호출 안함.
+    std::vector<A> testA(10, 42);
     std::cout << std::endl;
-}  // 소멸자 호출 10회
-
-{   
-    std::vector<A> alist(10, 5);  // 5 -> A(5), 암시적 생성자 호출. 소멸자 호출.
-    alist.reserve(20);  // 소멸자 호출 10회, 생성자 호출 안함.
+    testA.reserve(20);
     std::cout << std::endl;
-}  // 소멸자 호출 10회
-
-{   
-    std::vector<B> blist(10, 5);
-    blist.reserve(20);
+    
+    testA.insert(testA.end()-1, 5, 21); // 1.복사생성자 2.복사대입
     std::cout << std::endl;
-}
-
-{   
-    std::vector<A> blist(10, 5);
-    blist.reserve(20);
-    std::cout << std::endl;
-}
-
 }

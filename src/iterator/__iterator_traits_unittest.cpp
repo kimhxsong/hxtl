6d684d00c251e8a8d4a883acbf1d566f86419e71
kimhxsong <kimhxsong@gmail.com>
2022-08-05 19:55:17 +0900

    Add __iterator_traits_unittest.cpp

---
+++ src/iterator/__iterator_traits_unittest.cpp
@@ -0,0 +1,53 @@
+#include "__iterator_traits.hpp"
+
+#include <string>
+#include <typeinfo>
+#include <iterator>
+
+#include <vector>
+
+#include "gtest/gtest.h"
+
+TEST(iterator_traits, default_template) {
+  EXPECT_EQ(typeid(std::iterator_traits<std::vector<int>::iterator>::difference_type).name(),
+            typeid(ft::iterator_traits<std::vector<int>::iterator>::difference_type).name());
+  EXPECT_EQ(typeid(std::iterator_traits<std::vector<int>::iterator>::value_type).name(),
+            typeid(ft::iterator_traits<std::vector<int>::iterator>::value_type).name());
+  EXPECT_EQ(typeid(std::iterator_traits<std::vector<int>::iterator>::pointer).name(),
+            typeid(ft::iterator_traits<std::vector<int>::iterator>::pointer).name());
+  EXPECT_EQ(typeid(std::iterator_traits<std::vector<int>::iterator>::reference).name(),
+            typeid(ft::iterator_traits<std::vector<int>::iterator>::reference).name());
+  EXPECT_EQ(typeid(std::iterator_traits<std::vector<int>::iterator>::iterator_category).name(),
+            typeid(ft::iterator_traits<std::vector<int>::iterator>::iterator_category).name());
+};
+
+TEST(iterator_traits, pointer_template) {
+  EXPECT_EQ(typeid(std::iterator_traits<int*>::difference_type).name(),
+            typeid(ft::iterator_traits<int*>::difference_type).name());
+  EXPECT_EQ(typeid(std::iterator_traits<int*>::value_type).name(),
+            typeid(ft::iterator_traits<int*>::value_type).name());
+  EXPECT_EQ(typeid(std::iterator_traits<int*>::pointer).name(),
+            typeid(ft::iterator_traits<int*>::pointer).name());
+  EXPECT_EQ(typeid(std::iterator_traits<int*>::reference).name(),
+            typeid(ft::iterator_traits<int*>::reference).name());
+  EXPECT_EQ(typeid(std::iterator_traits<int*>::iterator_category).name(),
+            typeid(ft::iterator_traits<int*>::iterator_category).name());
+};
+
+TEST(iterator_traits, const_pointer_template) {
+  EXPECT_EQ(typeid(std::iterator_traits<const int*>::difference_type).name(),
+            typeid(ft::iterator_traits<const int*>::difference_type).name());
+  EXPECT_EQ(typeid(std::iterator_traits<const int*>::value_type).name(),
+            typeid(ft::iterator_traits<const int*>::value_type).name());
+  EXPECT_EQ(typeid(std::iterator_traits<const int*>::pointer).name(),
+            typeid(ft::iterator_traits<const int*>::pointer).name());
+  EXPECT_EQ(typeid(std::iterator_traits<const int*>::reference).name(),
+            typeid(ft::iterator_traits<const int*>::reference).name());
+  EXPECT_EQ(typeid(std::iterator_traits<const int*>::iterator_category).name(),
+            typeid(ft::iterator_traits<const int*>::iterator_category).name());
+};
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
--- src/iterator/iterator.hpp
+++ src/iterator/iterator.hpp
@@ -1,7 +1,7 @@
 #ifndef ITERATOR_HPP_
 #define ITERATOR_HPP_
 
-#include "./iterator/iterator_traits.hpp"
-#include "./iterator/reverse_iterator.hpp"
+#include "__iterator_traits.hpp"
+#include "__reverse_iterator.hpp"
 
 #endif  // ITERATOR_HPP_

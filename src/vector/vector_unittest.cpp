5ad104d241d3693f216adad9f5513f4541ac8055
kimhxsong <kimhxsong@gmail.com>
2022-08-07 01:49:18 +0900

    Add test suites to vector_unittest.cpp

--- src/vector/vector_unittest.cpp
+++ src/vector/vector_unittest.cpp
@@ -15,13 +15,15 @@ TEST(vector_constructor, default_constructor) {
 
 TEST(vector_constructor, fill_constructor) {
   ft::vector<int> __ft__vec(10);
-  for (int i = 0; i < 10; i++) {
-    EXPECT_EQ(__ft__vec[i], 0);
+  std::vector<int> __std__vec(__ft__vec.size());
+  for (int i = 0; i < __ft__vec.size(); i++) {
+    EXPECT_EQ(__ft__vec[i], __std__vec[i]);
   }
 
   ft::vector<int> __ft__vec2(10, 42);
-  for (int i = 0; i < 10; i++) {
-    EXPECT_EQ(__ft__vec2[i], 42);
+  std::vector<int> __std__vec2(__ft__vec2.size(), 42);
+  for (int i = 0; i < __ft__vec2.size(); i++) {
+    EXPECT_EQ(__ft__vec2[i], __std__vec2[i]);
   }
 }
 
@@ -39,141 +41,288 @@ TEST(vector_constructor, range_constructor) {
 TEST(vector_constructor, copy_constructor) {
   ft::vector<int> __ft__vec(10, 42);
   ft::vector<int> __ft__vec2(__ft__vec);
+  std::vector<int> __std__vec(__ft__vec.size(), 42);
+
   for (int i = 0; i < __ft__vec.size(); i++) {
-    EXPECT_EQ(__ft__vec2[i], 42);
+    EXPECT_EQ(__ft__vec2[i], __std__vec[i]);
   }
 }
 
 TEST(vector_iterator, iterator) {
   ft::vector<int> __ft__vec(10);
-  __ft__vec.begin();
-  __ft__vec.end();
+  ft::vector<std::string> __ft__vec2(1, "Hello");
+
+  std::vector<int> __std__vec(10);
+  std::vector<std::string> __std__vec2(1, "Hello");
+
+  EXPECT_EQ(*__ft__vec2.begin(), *__std__vec2.begin());
+  EXPECT_EQ(__ft__vec2.begin()->length(), __std__vec2.begin()->length());
+
+  EXPECT_EQ(*++__ft__vec.begin(), *++__std__vec.begin());
+  EXPECT_EQ(*__ft__vec.begin()++, *__std__vec.begin()++);
+  EXPECT_EQ(__ft__vec.begin()[0], __std__vec.begin()[0]);
+  
+  --__ft__vec.end();
+  __ft__vec.end()--;
+
+  EXPECT_EQ(__ft__vec.begin() == __ft__vec.begin(), __std__vec.begin() == __std__vec.begin());
+  EXPECT_EQ(__ft__vec.begin() == __ft__vec.end(), __std__vec.begin() == __std__vec.end());
+
+  EXPECT_EQ(__ft__vec.begin() != __ft__vec.begin(), __std__vec.begin() != __std__vec.begin());
+  EXPECT_EQ(__ft__vec.begin() != __ft__vec.end(), __std__vec.begin() != __std__vec.end());
+
+  EXPECT_EQ(__ft__vec.begin() < __ft__vec.begin(), __std__vec.begin() < __std__vec.begin());
+  EXPECT_EQ(__ft__vec.begin() < __ft__vec.end(), __std__vec.begin() < __std__vec.end());
+
+  EXPECT_EQ((__ft__vec.begin()+1) <= __ft__vec.begin(), (__std__vec.begin()+1) <= __std__vec.begin());
+  EXPECT_EQ(__ft__vec.begin() <= __ft__vec.end(), __std__vec.begin() <= __std__vec.end());
+
+  EXPECT_EQ(__ft__vec.begin() > __ft__vec.begin(), __std__vec.begin() > __std__vec.begin());
+  EXPECT_EQ(__ft__vec.begin() > __ft__vec.end(), __std__vec.begin() > __std__vec.end());
+
+  EXPECT_EQ((__ft__vec.begin()+1) >= __ft__vec.begin(), (__std__vec.begin()+1) >= __std__vec.begin());
+  EXPECT_EQ(__ft__vec.begin() >= __ft__vec.end(), __std__vec.begin() >= __std__vec.end());
+
+  EXPECT_EQ(*(__ft__vec.begin() + 3), *(__std__vec.begin() + 3));
+  EXPECT_EQ(*(3 + __ft__vec.begin()), *(3 + __ft__vec.begin()));
+
+  EXPECT_EQ(__ft__vec.end() - __ft__vec.begin(), __std__vec.end() - __std__vec.begin());
+
+  ft::vector<int>::iterator __ft__it = __ft__vec.begin();
+  std::vector<int>::iterator __std__it = __std__vec.begin();
+  __ft__it += 3;
+  __std__it += 3;
+  EXPECT_EQ(*__ft__it, *__std__it);
+  __ft__it -= 2;
+  __std__it -= 2;
+  EXPECT_EQ(*__ft__it, *__std__it);
 }
-
-
-// // ITERATORS
-// TEST(map_iterator, bidirectional_iterator) {
-//   ft::map<std::string,int> __ft__map;
-//   __ft__map["a"] = 10;
-//   __ft__map["b"] = 20;
-//   __ft__map["c"] = 30;
-//   __ft__map["d"] = 40;
-
-//   // Is default-constructible, copy-constructible, copy-assignable and destructible
-//   ft::map<std::string,int>::iterator __ft__it = __ft__map.begin();
-//   ft::map<std::string,int>::iterator __ft__it2(__ft__it);
-//   ft::map<std::string,int>::iterator __ft__it3;
-//   __ft__it3 = __ft__it2;
-
-//   // Can be compared for equivalence using the equality/inequality operators
-//   EXPECT_EQ(__ft__it2 == __ft__it3, true);
-//   EXPECT_EQ(__ft__it2 != __ft__map.end(), true);
-
-//   // Can be dereferenced as an rvalue (if in a dereferenceable state).
-//   EXPECT_EQ((*__ft__it2).second == 10, true);
-//   EXPECT_EQ(__ft__map.begin()->first.size() == std::string("a").size(), true);
-
-//   // For mutable iterators (non-constant iterators):
-//   // Can be dereferenced as an lvalue (if in a dereferenceable state).
-//   __ft__it2->second = 100;
-//   EXPECT_EQ(__ft__it2->second == 100, true);
-//   __ft__it2->second = 10;
-
-//   // Can be incremented (if in a dereferenceable state).
-//   // The result is either also dereferenceable or a past-the-end iterator.
-//   // Two iterators that compare equal, keep comparing equal after being both increased.
-//   ++__ft__it;
-//   __ft__it++;
-//   EXPECT_EQ((*__ft__it++).second == 30, true);
-//   EXPECT_EQ((__ft__it)->second == 40, true);
-
-//   --__ft__it;
-//   __ft__it--;
-//   EXPECT_EQ((*__ft__it--).second == 20, true);
-//   EXPECT_EQ((__ft__it)->second == 10, true);
-// }
 
 TEST(vector_iterator, reverse_iterator) {
-  ft::vector<int> __ft__vec(10);
-  __ft__vec.rbegin();
-  __ft__vec.rend();
+ft::vector<int> __ft__vec(10);
+  ft::vector<std::string> __ft__vec2(1, "Hello");
+
+  std::vector<int> __std__vec(10);
+  std::vector<std::string> __std__vec2(1, "Hello");
+
+  EXPECT_EQ(*__ft__vec2.rbegin(), *__std__vec2.rbegin());
+  EXPECT_EQ(__ft__vec2.rbegin()->length(), __std__vec2.rbegin()->length());
+
+  EXPECT_EQ(*++__ft__vec.rbegin(), *++__std__vec.rbegin());
+  EXPECT_EQ(*__ft__vec.rbegin()++, *__std__vec.rbegin()++);
+  EXPECT_EQ(__ft__vec.rbegin()[0], __std__vec.rbegin()[0]);
+  
+  --__ft__vec.rend();
+  __ft__vec.rend()--;
+
+  EXPECT_EQ(__ft__vec.rbegin() == __ft__vec.rbegin(), __std__vec.rbegin() == __std__vec.rbegin());
+  EXPECT_EQ(__ft__vec.rbegin() == __ft__vec.rend(), __std__vec.rbegin() == __std__vec.rend());
+
+  EXPECT_EQ(__ft__vec.rbegin() != __ft__vec.rbegin(), __std__vec.rbegin() != __std__vec.rbegin());
+  EXPECT_EQ(__ft__vec.rbegin() != __ft__vec.rend(), __std__vec.rbegin() != __std__vec.rend());
+
+  EXPECT_EQ(__ft__vec.rbegin() < __ft__vec.rbegin(), __std__vec.rbegin() < __std__vec.rbegin());
+  EXPECT_EQ(__ft__vec.rbegin() < __ft__vec.rend(), __std__vec.rbegin() < __std__vec.rend());
+
+  EXPECT_EQ((__ft__vec.rbegin()+1) <= __ft__vec.rbegin(), (__std__vec.rbegin()+1) <= __std__vec.rbegin());
+  EXPECT_EQ(__ft__vec.rbegin() <= __ft__vec.rend(), __std__vec.rbegin() <= __std__vec.rend());
+
+  EXPECT_EQ(__ft__vec.rbegin() > __ft__vec.rbegin(), __std__vec.rbegin() > __std__vec.rbegin());
+  EXPECT_EQ(__ft__vec.rbegin() > __ft__vec.rend(), __std__vec.rbegin() > __std__vec.rend());
+
+  EXPECT_EQ((__ft__vec.rbegin()+1) >= __ft__vec.rbegin(), (__std__vec.rbegin()+1) >= __std__vec.rbegin());
+  EXPECT_EQ(__ft__vec.rbegin() >= __ft__vec.rend(), __std__vec.rbegin() >= __std__vec.rend());
+
+  EXPECT_EQ(*(__ft__vec.rbegin() + 3), *(__std__vec.rbegin() + 3));
+  EXPECT_EQ(*(3 + __ft__vec.rbegin()), *(3 + __ft__vec.rbegin()));
+
+  EXPECT_EQ(__ft__vec.rend() - __ft__vec.rbegin(), __std__vec.rend() - __std__vec.rbegin());
+
+  ft::vector<int>::reverse_iterator __ft__it = __ft__vec.rbegin();
+  std::vector<int>::reverse_iterator __std__it = __std__vec.rbegin();
+  __ft__it += 3;
+  __std__it += 3;
+  EXPECT_EQ(*__ft__it, *__std__it);
+  __ft__it -= 2;
+  __std__it -= 2;
+  EXPECT_EQ(*__ft__it, *__std__it);
 }
 
-TEST(vector_iterator, convertible_to_const_iterator) {
-  ft::vector<int>::iterator it;
-  ft::vector<int>::const_iterator cit(it);
+TEST(vector_iterator, convertible_to_const) {
+  ft::vector<int> __ft__vec;
+  ft::vector<int>::iterator __ft__it(__ft__vec.begin());
+  ft::vector<int>::const_iterator __ft__cit(__ft__it);
+  ft::vector<int>::const_iterator __ft__cit2(__ft__cit);
+  // ft::vector<int>::iterator __ft__it2(__ft__cit2);  // !NOTE: Does Not Compile
+
+  ft::vector<int>::reverse_iterator __ft__rit(__ft__it);
+  ft::vector<int>::const_reverse_iterator __std__crit(__ft__rit);
+  ft::vector<int>::const_reverse_iterator __std__crit2(__std__crit);
+  ft::vector<int>::const_reverse_iterator __std__crit3(__ft__it);
+  ft::vector<int>::const_reverse_iterator __std__crit4(__ft__cit);
+  // ft::vector<int>::reverse_iterator __ft__rit2(__std__crit); !NOTE: Does Not Compile
 }
 
 TEST(vector_capacity, capacity) {
   ft::vector<int> __ft__vec(10);
-  __ft__vec.size();
+  EXPECT_EQ(__ft__vec.size(), 10);
   __ft__vec.max_size();
   __ft__vec.resize(20);
-  __ft__vec.empty();
+  EXPECT_EQ(__ft__vec.size(), 20);
+  EXPECT_EQ(__ft__vec.empty(), false);
+
   __ft__vec.reserve(40);
+  EXPECT_EQ(__ft__vec.size(), 40);  // TODO
+  //   vector_unittest.cpp:72: Failure
+  // Expected equality of these values:
+  //   __ft__vec.size()
+  //     Which is: 20
+  //   40
+
+  __ft__vec.reserve(30);
+  EXPECT_EQ(__ft__vec.size(), 40);
+  //   vector_unittest.cpp:74: Failure
+  // Expected equality of these values:
+  //   __ft__vec.size()
+  //     Which is: 20
+  //   40
+  __ft__vec.resize(30);
+  EXPECT_EQ(__ft__vec.size(), 30);
 }
 
 TEST(vector_elementaceess, element_access) {
   ft::vector<int> __ft__vec(10);
-  __ft__vec[0];
-  __ft__vec.at(0);
-  __ft__vec.front();
-  __ft__vec.back();
+  std::vector<int> __std__vec(10);
+  for (int i = 0; i < 10; i++) {
+    __ft__vec.push_back(i);
+    __std__vec.push_back(i);
+  }
+
+  EXPECT_EQ(__ft__vec[0], __std__vec[0]);
+  EXPECT_EQ(__ft__vec.at(1), __std__vec.at(1));  // TODO
+  // vector_unittest.cpp:112: Failure
+  // Expected equality of these values:
+  //   __ft__vec.at(1)
+  //     Which is: -1879048192
+  //   __std__vec.at(1)
+  //     Which is: 0
+
+  // __ft__vec.at(__ft__vec.capacity());
+  // unknown file: Failure
+  // C++ exception with description "at" thrown in the test body.
+
+  try {
+    __ft__vec.at(__ft__vec.capacity());
+  } catch (const std::out_of_range& oor) {
+  }
+
+  EXPECT_EQ(__ft__vec.front(), __std__vec.front());
+  EXPECT_EQ(__ft__vec.back(), __std__vec.back());
 }
 
 TEST(vector_modifiers, assign) {
-  ft::vector<int> __ft__vec(10);
+  std::vector<int> __std__vec(10, 42);
+  std::vector<int> __std__vec2(20, 5);
+  ft::vector<int> __ft__vec(10, 42);
   ft::vector<int> __ft__vec2;
+
   __ft__vec2.assign(__ft__vec.begin(), __ft__vec.end()); // range(1)
+  for (int i = 0; i < __std__vec.size(); i++) {
+    EXPECT_EQ(__std__vec[i], __ft__vec2[i]);
+  }
+
   __ft__vec2.assign(20, 5);  // fill(2)
+  for (int i = 0; i < __std__vec2.size(); i++) {
+    EXPECT_EQ(__std__vec2[i], __ft__vec2[i]);
+  }
 }
 
 TEST(vector_modifiers, push_back) {
   ft::vector<int> __ft__vec;
-  __ft__vec.push_back(10);
+  std::vector<int> __std__vec;
+
+  srand(0);
+  for (int i = 0; i < 5; i++)  {
+    int random_value = rand() % 10;
+    __ft__vec.push_back(random_value);
+    __std__vec.push_back(random_value);
+  }
+
+  for (int i = 0; i < 5; i++)  {
+    EXPECT_EQ(__ft__vec[i], __std__vec[i]);
+  }
 }
 
 TEST(vector_modifiers, pop_back) {
   ft::vector<int> __ft__vec(10);
   __ft__vec.pop_back();
+  EXPECT_EQ(__ft__vec.size(), 9);
+  for (size_t size = __ft__vec.size(); size > 0; size--) {
+    __ft__vec.pop_back();
+  }
+  EXPECT_EQ(__ft__vec.size(), 0);
+
+  // std::vector<int> __std__vec;
+  // ft::vector<int> __ft__vec2;
+  // __ft__vec2.pop_back();
+  // __std__vec.pop_back();
+  // EXPECT_EQ(__ft__vec2.size(), __std__vec.size());
+  // Expected equality of these values:
+  // __ft__vec2.size()
+  //   Which is: 4294967295
+  // __std__vec.size()
+  //   Which is: 18446744073709551615
+
 }
 
 TEST(vector_modifiers, insert) {
   ft::vector<int> __ft__vec;
   ft::vector<int> __ft__vec2;
 
+  std::vector<int> __std__vec;
+  std::vector<int> __std__vec2;
+
   __ft__vec.insert(__ft__vec.begin(), 5);  // single element(1)
-  EXPECT_EQ(__ft__vec[0], 5);
-  EXPECT_EQ(__ft__vec.size(), 1);
+  __std__vec.insert(__std__vec.begin(), 5);  // single element(1)
+  EXPECT_EQ(__ft__vec[0], __std__vec[0]);
+  EXPECT_EQ(__ft__vec.size(), __std__vec.size());
 
   __ft__vec.insert(__ft__vec.begin(), 10, 42);  // fill(2)
-  EXPECT_EQ(__ft__vec[0], 42);
-  EXPECT_EQ(__ft__vec[9], 42);
-  EXPECT_EQ(__ft__vec[10], 5);
-  EXPECT_EQ(__ft__vec.size(), 11);
+  EXPECT_EQ(__ft__vec.size(), __std__vec.size());
+  EXPECT_EQ(__ft__vec[0], __std__vec[0]);
+  EXPECT_EQ(__ft__vec[9], __std__vec[9]);
+  EXPECT_EQ(__ft__vec[10], __std__vec[10]);  // TODO
 
   __ft__vec2.insert(__ft__vec2.begin(), __ft__vec.begin(), __ft__vec.end());  // range(3)
-  EXPECT_EQ(__ft__vec2[0], 42);
-  EXPECT_EQ(__ft__vec2[9], 42);
-  EXPECT_EQ(__ft__vec2[10], 5);
-  EXPECT_EQ(__ft__vec2.size(), 11);
+  __std__vec2.insert(__std__vec2.begin(), __std__vec.begin(), __std__vec.end());
+  EXPECT_EQ(__ft__vec2[0], __std__vec2[0]);
+  EXPECT_EQ(__ft__vec2[9], __std__vec2[9]);
+  EXPECT_EQ(__ft__vec2[10], __std__vec2[10]);  // TODO
+  EXPECT_EQ(__ft__vec2.size(), __std__vec2.size());
 }
 
 TEST(vector_modifiers, erase) {
   ft::vector<int> __ft__vec;
+  std::vector<int> __std__vec;
+
   for (int i = 0; i < 10; i++) {
     __ft__vec.push_back(i);
+    __std__vec.push_back(i);
   }
+
   __ft__vec.erase(__ft__vec.begin());
-  EXPECT_EQ(__ft__vec[0], 1);
-  EXPECT_EQ(__ft__vec.size(), 9);
+  __std__vec.erase(__std__vec.begin());
+  EXPECT_EQ(__ft__vec[0], __std__vec[0]);  // TODO
+  EXPECT_EQ(__ft__vec.size(), __std__vec.size());
 
   __ft__vec.erase(__ft__vec.begin()+1, __ft__vec.begin()+3);
-  EXPECT_EQ(__ft__vec[1], 4);
-  EXPECT_EQ(__ft__vec.size(), 7);
+  __std__vec.erase(__std__vec.begin()+1, __std__vec.begin()+3);
+  EXPECT_EQ(__ft__vec[1], __std__vec[1]);  // TODO
+  EXPECT_EQ(__ft__vec.size(), __std__vec.size());
 
   __ft__vec.erase(__ft__vec.begin(), __ft__vec.end());
-  EXPECT_EQ(__ft__vec.size(), 0);
+  __std__vec.erase(__std__vec.begin(), __std__vec.end());
+  EXPECT_EQ(__ft__vec.size(), __std__vec.size());
 }
 
 TEST(vector_modifiers, swap) {

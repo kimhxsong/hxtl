6dd3823a735b72d5e65fa4b98538e7fc0983d0f0
kimhxsong <kimhxsong@gmail.com>
2022-08-25 00:24:43 +0900

    Fix vector_iterator

--- src/vector/vector_iterator.hpp
+++ src/vector/vector_iterator.hpp
@@ -8,14 +8,22 @@
 namespace ft {
 
 template <class T>
+class vector_iterator;
+
+template <class T>
+vector_iterator<T> operator+(int n, const vector_iterator<T>& it);
+template <class T>
+std::ostream& operator<<(std::ostream& os, const vector_iterator<T>& it);
+
+template <class T>
 class vector_iterator {
  public:
-  typedef typename iterator_traits<T>::iterator_category iterator_category;
-  typedef typename iterator_traits<T>::value_type value_type;
-  typedef typename iterator_traits<T>::difference_type difference_type;
-  typedef typename iterator_traits<T>::pointer pointer;
+  typedef typename ft::iterator_traits<T>::iterator_category iterator_category;
+  typedef typename ft::iterator_traits<T>::value_type value_type;
+  typedef typename ft::iterator_traits<T>::difference_type difference_type;
+  typedef typename ft::iterator_traits<T>::pointer pointer;
+  typedef typename ft::iterator_traits<T>::reference reference;
   typedef const pointer const_pointer;
-  typedef typename iterator_traits<T>::reference reference;
   typedef const reference const_reference;
 
   vector_iterator();
@@ -37,13 +45,13 @@ class vector_iterator {
   vector_iterator& operator--();
   vector_iterator operator--(int);
 
-  bool operator==(const vector_iterator& other);
-  bool operator!=(const vector_iterator& other);
+  bool operator==(const vector_iterator& other) const;
+  bool operator!=(const vector_iterator& other) const;
 
-  bool operator<(const vector_iterator& other);
-  bool operator>(const vector_iterator& other);
-  bool operator<=(const vector_iterator& other);
-  bool operator>=(const vector_iterator& other);
+  bool operator<(const vector_iterator& other) const;
+  bool operator>(const vector_iterator& other) const;
+  bool operator<=(const vector_iterator& other) const;
+  bool operator>=(const vector_iterator& other) const;
 
   vector_iterator operator+(difference_type n) const;
   vector_iterator operator-(difference_type n) const;
@@ -53,24 +61,17 @@ class vector_iterator {
   vector_iterator& operator+=(difference_type n);
   vector_iterator& operator-=(difference_type n);
 
-  reference operator[](difference_type n) const;
+  reference operator[](difference_type n);
+  const_reference operator[](difference_type n) const;
 
  private:
   pointer p_;
 };
 
 template <class T>
-vector_iterator<T> operator+(int n, const vector_iterator<T>& it);
-
-template <class T>
-std::ostream& operator<<(std::ostream& os, const vector_iterator<T>& it);
-
-template <class T>
 vector_iterator<T>::vector_iterator() : p_(0) {}
-
 template <class T>
 vector_iterator<T>::vector_iterator(pointer p) : p_(p) {}
-
 template <class T>
 vector_iterator<T>::vector_iterator(const vector_iterator<value_type*>& other) : p_(&*other) {}
 
@@ -87,17 +88,14 @@ template <class T>
 typename vector_iterator<T>::reference vector_iterator<T>::operator*() {
   return *p_;
 }
-
 template <class T>
 typename vector_iterator<T>::const_reference vector_iterator<T>::operator*() const {
   return *p_;
 }
-
 template <class T>
 typename vector_iterator<T>::pointer vector_iterator<T>::operator->() {
   return p_;
 }
-
 template <class T>
 typename vector_iterator<T>::const_pointer vector_iterator<T>::operator->() const {
   return p_;
@@ -108,20 +106,17 @@ vector_iterator<T>& vector_iterator<T>::operator++() {
   ++p_;
   return *this;
 }
-
 template <class T>
 vector_iterator<T> vector_iterator<T>::operator++(int) {
   vector_iterator<T> tmp(*this);
   p_++;
   return tmp;
 }
-
 template <class T>
 vector_iterator<T>& vector_iterator<T>::operator--() {
   --p_;
   return *this;
 }
-
 template <class T>
 vector_iterator<T> vector_iterator<T>::operator--(int) {
   vector_iterator<T> tmp(*this);
@@ -130,32 +125,28 @@ vector_iterator<T> vector_iterator<T>::operator--(int) {
 }
 
 template <class T>
-bool vector_iterator<T>::operator==(const vector_iterator<T>& other) {
+bool vector_iterator<T>::operator==(const vector_iterator<T>& other) const {
   return p_ == &*other;
 }
-
 template <class T>
-bool vector_iterator<T>::operator!=(const vector_iterator<T>& other) {
+bool vector_iterator<T>::operator!=(const vector_iterator<T>& other) const {
   return p_ != &*other;
 }
 
 template <class T>
-bool vector_iterator<T>::operator<(const vector_iterator<T>& other) {
+bool vector_iterator<T>::operator<(const vector_iterator<T>& other) const {
   return p_ < &*other;
 }
-
 template <class T>
-bool vector_iterator<T>::operator>(const vector_iterator<T>& other) {
+bool vector_iterator<T>::operator>(const vector_iterator<T>& other) const {
   return p_ > &*other;
 }
-
 template <class T>
-bool vector_iterator<T>::operator<=(const vector_iterator<T>& other) {
+bool vector_iterator<T>::operator<=(const vector_iterator<T>& other) const {
   return p_ <= &*other;
 }
-
 template <class T>
-bool vector_iterator<T>::operator>=(const vector_iterator<T>& other) {
+bool vector_iterator<T>::operator>=(const vector_iterator<T>& other) const {
   return p_ >= &*other;
 }
 
@@ -164,7 +155,6 @@ vector_iterator<T> vector_iterator<T>::operator+(difference_type n) const {
   vector_iterator<pointer> tmp(p_ + n);
   return tmp;
 }
-
 template <class T>
 vector_iterator<T> vector_iterator<T>::operator-(difference_type n) const {
   vector_iterator<pointer> tmp(p_ - n);
@@ -182,7 +172,6 @@ vector_iterator<T>& vector_iterator<T>::operator+=(difference_type n) {
   p_ += n;
   return *this;
 }
-
 template <class T>
 vector_iterator<T>& vector_iterator<T>::operator-=(difference_type n) {
   p_ -= n;
@@ -190,7 +179,12 @@ vector_iterator<T>& vector_iterator<T>::operator-=(difference_type n) {
 }
 
 template <class T>
-typename vector_iterator<T>::reference vector_iterator<T>::operator[](difference_type n) const {
+typename vector_iterator<T>::reference vector_iterator<T>::operator[](difference_type n) {
+  return *(p_ + n);
+}
+template <class T>
+typename vector_iterator<T>::const_reference vector_iterator<T>::operator[](
+    difference_type n) const {
   return *(p_ + n);
 }
 
